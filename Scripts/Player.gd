adf58ed316a1da5c43b3373a9a6c9fd5e2dc17c6
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-12-01 03:11:58 -0600

    Made little map and movement works

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,19 +1,23 @@
 extends KinematicBody2D
 
 export var friction := 0.5
-export var max_speed := 250.0
-export var acceleration_speed := 100.0
-export var jump_speed := 500.0
-export var gravity := 25.0
+export var max_speed := 100.0
+export var acceleration_speed := 25.0
+export var jump_speed := 250.0
+export var gravity := 20.0
+export var snapping_distance := 10.0
 
 var velocity := Vector2.ZERO
 var acceleration := Vector2.ZERO
+var snapping := Vector2.DOWN
+var jumping := false
 
 
-func _process(_delta):
+func _physics_process(_delta):
 	_apply_friction()
 	_get_acceleration()
 	_jump()
+	_set_snapping()
 	_apply_gravity()
 	_update_velocity()
 	_apply_velocity()
@@ -21,6 +25,9 @@ func _process(_delta):
 
 func _apply_friction():
 	velocity.x *= friction
+
+	if get_floor_normal().x != 0 and get_floor_normal().y != 0:
+		velocity.x = 0
 
 
 func _get_acceleration():
@@ -36,6 +43,13 @@ func _get_acceleration():
 func _jump():
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		acceleration.y = -jump_speed
+		jumping = true
+	elif is_on_floor():
+		jumping = false
+
+
+func _set_snapping():
+	snapping = Vector2.ZERO if jumping else Vector2.DOWN
 
 
 func _apply_gravity():
@@ -48,4 +62,4 @@ func _update_velocity():
 
 
 func _apply_velocity():
-	velocity = move_and_slide(velocity, Vector2.UP, true)
+	velocity = move_and_slide_with_snap(velocity, snapping * snapping_distance, Vector2.UP, true)
