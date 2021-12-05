a51a5aabb87d4f1cccf97653ec6dbf9d19d2a20c
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-12-04 23:36:39 -0600

    Finished bargaining and started depression

--- Scripts/GameObjects/RunnerArea.gd
+++ Scripts/GameObjects/RunnerArea.gd
@@ -3,11 +3,7 @@ extends Area2D
 export(String, "right", "left", "jump") var command = "right"
 
 
-func _ready():
-# warning-ignore:return_value_discarded
-	connect("body_entered", self, "_on_body_entered")
-
-
-func _on_body_entered(body):
-	if body.is_in_group("runner"):
-		body.call(command)
+func _physics_process(_delta):
+	for body in get_overlapping_bodies():
+		if body.is_in_group("runner"):
+			body.call(command)
