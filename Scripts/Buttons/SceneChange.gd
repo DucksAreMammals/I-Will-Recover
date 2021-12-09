35173798de5064f5b4ed5cf4dfe5eb3e0d076e54
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-12-08 22:10:47 -0600

    Added menu button from settings

--- Scripts/Buttons/SceneChange.gd
+++ Scripts/Buttons/SceneChange.gd
@@ -4,5 +4,8 @@ export(String, FILE) var scene
 
 
 func _pressed():
+	FadeScreen.fade_in()
+	FadeScreen.set_text("")
+	yield(FadeScreen, "AnimationFinished")
 #warning-ignore:return_value_discarded
 	get_tree().change_scene(scene)
--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -3,6 +3,13 @@ extends Control
 
 func _ready():
 	$MusicSlider.value = Global.music_volume
+	$MusicSlider.grab_focus()
+	FadeScreen.fade_out_fast()
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("pause"):
+		$MenuButton._pressed()
 
 
 func _on_MusicSlider_value_changed(value):
